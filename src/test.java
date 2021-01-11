ab1de94345b1803890476f5cc16b1fc4c283ca94
Snagrap <karlkarilaid04@gmail.com>
2021-01-11 15:11:04 +0200

    Teksti väljastamise näide

--- src/test.java
+++ src/test.java
@@ -1,24 +1,13 @@
 /*
- *massiivid
- * autor Karl Karilaid
- *
- */
+ * Teksti väljastamise näide
+ * autor - Karl Karilaid
+ * ülesanne 1
+ * */
 
 public class test {
     public static void main(String[] args) {
-        int[] numbrid = new int[10];
-        numbrid[0] = 1;
-        numbrid[1] = 2;
-        numbrid[2] = 3;
-        numbrid[3] = 4;
-        numbrid[4] = 5;
-        System.out.println(numbrid.length);
-        for (int i = 0; i < numbrid.length; i++) {
-            System.out.println(numbrid[i]);
-        }
-        numbrid[5] = 6;
-        System.out.println(numbrid[5]);
+        // väljastamine
+        System.out.println("1. rida\n");
+        System.out.println("2. rida");
     }
-
-
 }
