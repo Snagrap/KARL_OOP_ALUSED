8e0e881a526dcd56f3c115dc5fdbe34f6d28ba7f
Snagrap <karlkarilaid04@gmail.com>
2021-01-11 14:59:30 +0200

    Massiivid . [] ja {} abil

--- src/test.java
+++ src/test.java
@@ -1,12 +1,19 @@
 public class test {
     public static void main(String[] args) {
-        int[] numbrid = {1, 2, 3, 4, 5};
+        int[] numbrid = new int[10];
+        numbrid[0] = 1;
+        numbrid[1] = 2;
+        numbrid[2] = 3;
+        numbrid[3] = 4;
+        numbrid[4] = 5;
         System.out.println(numbrid.length);
         System.out.println(numbrid[0]);
         System.out.println(numbrid[1]);
         System.out.println(numbrid[2]);
         System.out.println(numbrid[3]);
         System.out.println(numbrid[4]);
+        numbrid[5] = 6;
+        System.out.println(numbrid[5]);
     }
 
 
