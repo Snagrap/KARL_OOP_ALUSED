190c23a0b9cd663d964370a0181d5ae058ac7f10
Snagrap <karlkarilaid04@gmail.com>
2021-01-11 15:32:18 +0200

    if-else - ülesanne 2.1

--- src/test.java
+++ src/test.java
@@ -1,7 +1,7 @@
 /*
  * Muutujate kasutamise näide
  * autor - Karl Karilaid
- * ülesanne 1.3
+ * ülesanne 2.1
  * */
 
 import java.util.Scanner;
@@ -12,14 +12,14 @@ public class test {
         //lubame kasutajal sisestada midagi
         Scanner sisend = new Scanner(System.in);
         //selgitame kasutajale, mida tuleb sisestada
-        System.out.println("Sisestage astme alus: ");
+        System.out.println("Palun sisesta pilvede kõrgus: ");
         //salvestame kasutaja sisestatud väärtuse
-        int astmeAlus = sisend.nextInt();
-        System.out.println("Siestage astendaja: ");
-        int astendaja = sisend.nextInt();
-        //arvutamine
-        int tulemus = (int) Math.pow(astmeAlus, astendaja)
-        // väljastamine
-        System.out.println(tulemus);
+        double pilvedeKorgus = sisend.nextDouble();
+        //kontroll
+        if (pilvedeKorgus > 6.0) {
+            System.out.println("Need on ülemised pilved");
+        } else {
+            System.out.println("Need ei ole ülemised pilved");
+        }
     }
 }
