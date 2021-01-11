7465426f642cb258a9a571f8b6ed987c4a4ceb2e
Snagrap <karlkarilaid04@gmail.com>
2021-01-11 15:16:03 +0200

    Massiivid - Ülesanne 5.1

--- src/test.java
+++ src/test.java
@@ -1,13 +1,18 @@
 /*
- * Teksti väljastamise näide
+ * massiivid
  * autor - Karl Karilaid
- * ülesanne 1
+ * ülesanne 5.1
  * */
+
+import java.util.Scanner;
 
 public class test {
     public static void main(String[] args) {
-        // väljastamine
-        System.out.println("1. rida\n");
-        System.out.println("2. rida");
+        int[] vastuvoetud = {2803, 2626, 2359, 1927, 2236, 2281, 2394, 2484, 2468};
+        Scanner sisend = new Scanner(System.in);
+        System.out.println("Sisesta aasta: ");
+        int aasta = sisend.nextInt();
+        int massiiviIndeks = aasta - 2011;
+        System.out.println("Vastuvõetud on " + vastuvoetud[massiiviIndeks]);
     }
 }
