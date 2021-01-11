64c8475152a3cc1c84d66855faf1b032d009f426
Snagrap <karlkarilaid04@gmail.com>
2021-01-11 16:20:27 +0200

    tsüklid - ülesanne 3.2 (Jänesevanemate mure)

--- src/test.java
+++ src/test.java
@@ -1,28 +1,27 @@
 /*
- * Tsüklid
+ * Jänesevanemate mure
  * autor - Karl Karilaid
- * ülesanne 3.3
+ * ülesanne 3.2
  * */
 
 import java.util.Scanner;
 
 public class test {
     public static void main(String[] args) {
-        //defineerime muutujad
-        //lubame kasutajal sisestada midagi
         Scanner sisend = new Scanner(System.in);
-        //selgitame kasutajale, mida tuleb sisestada
-        System.out.println("Täringute arv: ");
-        //salvestame kasutaja sisestatud väärtuse
-        int taringuteArv = sisend.nextInt();
-        //korduv tegevus
-        //kontroll
-        while (true) {
-            int taring = (int) Math.round(Math.random() * 6);
-            System.out.println(taring);
-            if (taring == taringuteArv) {
-                break;
+        System.out.println("Sisesta ringide arv: ");
+        int ringideArv = sisend.nextInt();
+        int joostud = 0;
+        int porgandid = 0;
+        int mitu = 2;
+        while (joostud < ringideArv) {
+            if (joostud % 2 == 0) {
+                porgandid += mitu;
+                mitu += 2;
+
             }
+            joostud++;
         }
+        System.out.println(porgandid);
     }
 }
