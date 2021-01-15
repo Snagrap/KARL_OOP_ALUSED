61ebbdd9c9129132e6a621bef9652e49989572b7
Snagrap <karlkarilaid04@gmail.com>
2021-01-15 12:50:09 +0200

    funktsioonid - ülesanne 6.4a (Tervitused mõtisklusega)

--- src/test.java
+++ src/test.java
@@ -1,22 +1,28 @@
 /*
- * Õunamahla tegemine
+ * Tervitused mõtisklusega
  * autor - Karl Karilaid
- * ülesanne 6.2
+ * ülesanne 6.4a
  * */
 
 import java.util.Scanner;
 
 public class test {
-    static int mahlapakkidearv(double õun) {
-        int mahlapakkidearv = (int) Math.round(õun * 0.4 / 3);
-        return mahlapakkidearv;
+    static Object tervitus(int külalised) {
+        System.out.println("Võõrustaja: Tere!");
+        System.out.println("Täna " + külalised + ". korda tervitada, mõtiskleb võõrustaja.");
+        System.out.println("Külaline: Tere, suur tänu kutse eest!");
+        return null;
     }
 
     public static void main(String[] args) {
         Scanner sisend = new Scanner(System.in);
-        System.out.println("Sisestage õunte kogus kilogrammides: ");
-        double õun = sisend.nextDouble();
-        System.out.println(mahlapakkidearv(õun));
+        System.out.println("Sisestage külaliste arv: ");
+        int külalised = sisend.nextInt();
+        int kord = 1;
+        while (külalised >= kord) {
+            tervitus(kord);
+            kord++;
+        }
 
     }
 }
