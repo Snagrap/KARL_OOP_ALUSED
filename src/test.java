9627845788b1640af90f4bee5ff424a91f0577c2
Snagrap <karlkarilaid04@gmail.com>
2021-01-15 13:49:21 +0200

    nimekirjad - ülesanne (Hashtest)

--- src/test.java
+++ src/test.java
@@ -1,37 +1,26 @@
 /*
- * Tahvli juurde
+ * -
  * autor - Karl Karilaid
- * ülesanne 5.4c
+ * ülesanne -
  * */
 
-import java.io.File;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class test {
     public static void main(String[] args) {
-        ArrayList<String> opilased = new ArrayList<>();
-        File fail = new File("C:\\Users\\karl\\IdeaProjects\\oop_alused\\src\\nimekiri.txt");
-        Scanner sisendFailist = null;
-        try {
-            sisendFailist = new Scanner(fail);
-        } catch (Exception e) {
-            System.out.println("Faili pole - " + e.getMessage());
-        }
-        // loeme failist
-        while (sisendFailist.hasNextLine()) {
-            String rida = sisendFailist.nextLine();
-            opilased.add(rida); // lisame loetud väärtus nimekirja sisse
-        }
-        sisendFailist.close();
-        LocalDateTime tananeKuupaev = LocalDateTime.now();
-        System.out.println("Tänane kuupäev ilma vorminduseta: " + tananeKuupaev);
-        DateTimeFormatter kuupaevaVormindus = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:MM:ss");
-        String tananeKuupaevVormindatud = tananeKuupaev.format(kuupaevaVormindus);
-        System.out.println("Tänane vormindatud kuupäev " + tananeKuupaevVormindatud);
-        int indeks = Integer.parseInt(tananeKuupaevVormindatud);
-        System.out.println(opilased.get(indeks - 1));
+        HashMap<String, String> grupp = new HashMap<String, String>();
+        grupp.put("matti", "mage");
+        grupp.put("mikael", "mixu");
+        grupp.put("arto", "arppa");
+        /*
+        for (String nimi: grupp.keySet()) {
+            System.out.println(nimi + " - " + grupp.get(nimi));
+        }*/
+        Scanner sisend = new Scanner(System.in);
+        System.out.println("Sisesta nimi: ");
+        String nimi = sisend.nextLine();
+        nimi = nimi.toLowerCase();
+        System.out.println(nimi + " - " + grupp.get(nimi));
     }
 }
