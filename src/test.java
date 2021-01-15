ec9b1e5934da9b1e4a5a180d4ed4cc57795d5c3a
Snagrap <karlkarilaid04@gmail.com>
2021-01-15 12:40:17 +0200

    funktsioonid - ülesanne 6.2 (Õunamahla tegemine)

--- src/test.java
+++ src/test.java
@@ -1,29 +1,22 @@
 /*
- * Banner
+ * Õunamahla tegemine
  * autor - Karl Karilaid
- * ülesanne 6.1
+ * ülesanne 6.2
  * */
 
 import java.util.Scanner;
 
 public class test {
-    //eelarve meetod
-    static String banner(String lause) {
-        String suur_lause = lause.toUpperCase();
-        return suur_lause;
+    static int mahlapakkidearv(double õun) {
+        int mahlapakkidearv = (int) Math.round(õun * 0.4 / 3);
+        return mahlapakkidearv;
     }
 
     public static void main(String[] args) {
         Scanner sisend = new Scanner(System.in);
-        System.out.println("Mitu korda soovite lauset näidata: ");
-        int kord = sisend.nextInt();
-        sisend = new Scanner(System.in);
-        System.out.println("Mis lauset soovite näidata:");
-        String lause = sisend.nextLine();
-        while (kord > 0) {
-            System.out.println(banner(lause));
-            kord -= 1;
-        }
-    }
+        System.out.println("Sisestage õunte kogus kilogrammides: ");
+        double õun = sisend.nextDouble();
+        System.out.println(mahlapakkidearv(õun));
 
+    }
 }
