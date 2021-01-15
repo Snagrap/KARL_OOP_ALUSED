709fc9dc8c49c1517c7db06173d5d863fe8de987
Snagrap <karlkarilaid04@gmail.com>
2021-01-15 12:19:36 +0200

    funktsioonid - ülesanne 6.3 (peo eelarve)

--- src/test.java
+++ src/test.java
@@ -1,22 +1,26 @@
 /*
- * Male
+ * Peo eelarve
  * autor - Karl Karilaid
- * ülesanne 3.4c
+ * ülesanne 6.3
  * */
 
 import java.util.Scanner;
 
 public class test {
+    //eelarve meetod
+    static int eelarve(int kulalisteArv) {
+        int summa = kulalisteArv * 10 + 55;
+        return summa;
+    }
+
     public static void main(String[] args) {
         Scanner sisend = new Scanner(System.in);
-        System.out.println("Sisestage täisarv: ");
-        int ruutudeArv = sisend.nextInt();
-        int kord = 1;
-        int nisu = 1;
-        while (kord < ruutudeArv) {
-            nisu = nisu * 2;
-            kord++;
-        }
-        System.out.println("Nisuteri " + ruutudeArv + ". ruudu eest: " + nisu);
+        System.out.println("Mitu inimest on kutsutud: ");
+        int kutsutud = sisend.nextInt();
+        System.out.println("Mitu inimest tuleb peole:");
+        int kindel = sisend.nextInt();
+        System.out.println("Maksimaalne eelarve: " + eelarve(kutsutud));
+        System.out.println("Minimaalne eelarve: " + eelarve(kindel));
     }
+
 }
